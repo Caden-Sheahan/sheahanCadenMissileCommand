7340cd5874a57264d1a21c3f5b9c1f6202708605
Caden-Sheahan <sheahancj@outlook.com>
2022-09-11 17:03:49 -0500

    Begin Code!

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,31 @@
+/******************************************************************************
+//      File Name: GameController.cs
+//      Author: Caden Sheahan
+//      Creation Date: September 11th, 2022
+//
+//      Description: The GameController script controls inputs and actions that
+//      dont' effect the gameplay. It consists of debug inputs
+******************************************************************************/ 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.R)) // reset game
+        {
+            print("Reload Game!");
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKey(KeyCode.Escape)) // quit game (in build)
+        {
+            print("Quit!");
+            Application.Quit();
+        }
     }
 }
